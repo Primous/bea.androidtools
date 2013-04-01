69276c28adc376dc1085e9fbf38ae271190368fc
@brunojensen <@brunojensen>
2013-04-01 11:21:51 -0300

    Testa se possui conectividade ou se já esta conectado.
    Signed-off-by: @brunojensen <@brunojensen>

--- src/main/java/br/com/bea/androidtools/api/android/AndroidUtils.java
+++ src/main/java/br/com/bea/androidtools/api/android/AndroidUtils.java
@@ -19,4 +19,18 @@ public final class AndroidUtils {
     private static boolean hasConnectivity(final NetworkInfo info) {
         return null != info && info.isAvailable() && info.isConnected();
     }
+
+    public static final boolean isConnected(final Context context, final int... networkTypes) {
+        final ConnectivityManager connectivity = (ConnectivityManager) context
+            .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (networkTypes.length > 0)
+            for (final int networkType : networkTypes)
+                if (ConnectivityManager.isNetworkTypeValid(networkType))
+                    return isConnected(connectivity.getNetworkInfo(networkType));
+        return isConnected(connectivity.getActiveNetworkInfo());
+    }
+
+    private static boolean isConnected(final NetworkInfo info) {
+        return null != info && info.isConnected();
+    }
 }
